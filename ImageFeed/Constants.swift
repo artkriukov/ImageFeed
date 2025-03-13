dc4983adf45dc3b0c933a7ca325b068b27957f7d
Artem <art.kriukov@gmail.com>
2025-03-13 15:41:39 +0300

    feat: add API constants

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+enum Constants {
+    static let accessKey = "vTNkfKtDmoBndlU2TJ8gz_06HfMKyZA1fS84IJLEnjI"
+    static let secretKey = "f_vvH0rjVC6eyydk5w7RuPLH2Am-pxHfhgxNpEmBYvA"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
